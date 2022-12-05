a3bb5f568829a4cfd3548393ef4f7297c20eab0f
mjnoach <a.siennkiewicz@gmail.com>
2022-12-05 22:42:33 +0100

    create http server

---
+++ backend/cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello World!")
+	fmt.Println("Endpoint Hit: home")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", home)
+}
+
+func main() {
+	hostname := "localhost"
+	port := "8080"
+	host := fmt.Sprintf("%s:%s", hostname, port)
+
+	handleRequests()
+	fmt.Println("Starting server on " + host)
+	log.Fatal(http.ListenAndServe(host, nil))
+}
