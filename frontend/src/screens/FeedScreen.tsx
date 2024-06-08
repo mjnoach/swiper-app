17a326b870f26c39527e52ce862af8fdefb7ad73
mjnoach <a.siennkiewicz@gmail.com>
2024-06-09 01:30:25 +0200

    update feed screen

--- frontend/src/screens/FeedScreen.tsx
+++ frontend/src/screens/FeedScreen.tsx
@@ -6,22 +6,25 @@ import { SessionContext } from '../components/SessionContext'
 import { api } from '../lib/api'
 import { User } from '../types'
 
-const STACK_SIZE = 10
-
 export default function FeedScreen() {
-  const [profiles, setProfiles] = React.useState<User[]>([])
+  const [profiles, setProfiles] = React.useState<User[] | null>(null)
   const { user } = useContext(SessionContext)
 
   useEffect(() => {
-    if (!user) return setProfiles([])
-    api.fetchProfiles(user).then(({ data: profiles }) => {
-      setProfiles(profiles.slice(0, STACK_SIZE))
+    if (!user) return
+    api.fetchProfiles(user).then((res) => {
+      const { data: profiles } = res
+      if (profiles === null) {
+        // ran out of profiles
+        return
+      }
+      setProfiles(profiles)
     })
   }, [user])
 
   return (
     <View style={styles.container}>
-      <Deck items={profiles} />
+      {profiles && <Deck items={profiles} />}
     </View>
   )
 }
@@ -29,9 +32,6 @@ export default function FeedScreen() {
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lightblue',
-    cursor:
-      "url('https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/Ad1_-cursor.png') 39 39,\n    auto",
     alignItems: 'center',
     justifyContent: 'center',
   },
