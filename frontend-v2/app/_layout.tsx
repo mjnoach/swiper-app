7a0203436ade5688272880f4d13a206860be1645
mjnoach <a.siennkiewicz@gmail.com>
2024-06-09 16:22:34 +0200

    update layout

--- frontend-v2/app/_layout.tsx
+++ frontend-v2/app/_layout.tsx
@@ -1,9 +1,42 @@
 import { Stack } from "expo-router"
+import { Image, StyleSheet, Text, View } from "react-native"
+
+function Logo() {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        style={styles.image}
+        source={{ uri: "./assets/images/logo.png" }}
+      />
+      <Text style={styles.text}>Swiper App</Text>
+    </View>
+  )
+}
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      screenOptions={{
+        // headerStyle: { backgroundColor: "#eb5f5d" },
+        headerTitle: (props) => <Logo />,
+      }}
+    >
+      <Stack.Screen name="index" options={{}} />
+      <Stack.Screen name="user" />
     </Stack>
   )
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: 50,
+    height: 50,
+  },
+  text: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+})
--- frontend-v2/app/user.tsx
+++ frontend-v2/app/user.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native"
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>User</Text>
     </View>
   )
 }
