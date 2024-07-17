53f00373c70b9e6dce785ee753657c04ff768e32
mjnoach <a.siennkiewicz@gmail.com>
2024-07-17 22:13:19 +0200

    update db seed migration

--- backend/api/internal/db/migrations/2-seed.sql
+++ backend/api/internal/db/migrations/2-seed.sql
@@ -2,16 +2,15 @@
 
 INSERT INTO Users (id, email, password, name, gender, age)
 VALUES
-	(1, 'test@mail.com', 'test@mail.com', 'Taylor Johnson', 'female', 35),
-	(2, 'test1@mail.com', 'test1@mail.com', 'Casey Lee', 'female', 43),
-	(3, 'test2@mail.com', 'test2@mail.com', 'Drew Kim', 'male', 53),
-	(4, 'test3@mail.com', 'test3@mail.com', 'Riley Lewis', 'female', 44),
-	(5, 'test4@mail.com', 'test4@mail.com', 'Riley Lewis', 'male', 41),
-	(6, 'test5@mail.com', 'test5@mail.com', 'Casey Lee', 'female', 65),
-	(7, 'test6@mail.com', 'test6@mail.com', 'Drew Kim', 'male', 19),
-	(8, 'test7@mail.com', 'test7@mail.com', 'Morgan Davis', 'male', 58),
-	(9, 'test8@mail.com', 'test8@mail.com', 'Jamie Taylor', 'female', 54),
-	(10, 'test9@mail.com', 'test9@mail.com', 'Taylor Johnson', 'female', 29);
+	(1, 'test1@mail.com', 'test1@mail.com', 'Casey Lee', 'female', 43),
+	(2, 'test2@mail.com', 'test2@mail.com', 'Drew Kim', 'male', 53),
+	(3, 'test3@mail.com', 'test3@mail.com', 'Riley Lewis', 'female', 44),
+	(4, 'test4@mail.com', 'test4@mail.com', 'Riley Lewis', 'male', 41),
+	(5, 'test5@mail.com', 'test5@mail.com', 'Casey Lee', 'female', 65),
+	(6, 'test6@mail.com', 'test6@mail.com', 'Drew Kim', 'male', 19),
+	(7, 'test7@mail.com', 'test7@mail.com', 'Morgan Davis', 'male', 58),
+	(8, 'test8@mail.com', 'test8@mail.com', 'Jamie Taylor', 'female', 54),
+	(9, 'test9@mail.com', 'test9@mail.com', 'Taylor Johnson', 'female', 29);
 
 INSERT INTO Swipes (swipeFrom, swipedUser, preference)
 VALUES
@@ -22,7 +21,6 @@ VALUES
 	(6, 1, 'yes'),
 	(7, 1, 'no'),
 	(8, 1, 'no'),
-	(9, 1, 'yes'),
-	(10, 1, 'no');
+	(9, 1, 'yes');
 
 -- +migrate Down
